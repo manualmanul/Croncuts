fe444b84ee2cad4c4a9ae4bae691e4cd8bc8ec2f
Manual <am@catto.io>
2021-01-07 18:11:44 +0200

    Updated URLs

--- croncuts.sh
+++ croncuts.sh
@@ -11,10 +11,10 @@ apt install bluez git python3 python3-pip bluez-utils -y
 echo 'Installing pip3 dependencies'
 pip3 install pynput dbus-python
 
-echo 'Cloning SySS-Research/bluetooth-keyboard-emulator'
-git clone https://github.com/SySS-Research/bluetooth-keyboard-emulator.git
+echo 'Cloning manualmanul/Croncuts.git'
+git clone https://github.com/manualmanul/Croncuts.git
 
-echo 'Setting up SySS-Research/bluetooth-keyboard-emulator'
+echo 'Setting up manualmanul/Croncuts.git'
 cd bluetooth-keyboard-emulator
 sudo ./setup.sh
 
